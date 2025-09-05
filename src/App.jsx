6fd6f4c82c51141b66097d3cdeb5d22fb405abaa
Divine-Empire <mis@thedivineempire.com>
2025-09-05 17:05:08 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -63,7 +63,7 @@ function App() {
           <Route path="/accountabilityApprovals" element={<AccountablityApproval />} />
           <Route path="/calibrationCertificate" element={<CalibrationCertificate />} />
           <Route path="/conformation" element={<Conformation />} />
-          <Route path="/cancle" element={<Cancle />} />
+          <Route path="/cancle" element={<Cancel />} />
 
           </Route>
           <Route path="*" element={<NotFound />} />
